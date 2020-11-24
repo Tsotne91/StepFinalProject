37d4fabe4b91ea53de0eaef1ceeebeb037699ecb
tsotne <mail_to_tsotne@yahoo.com>
2020-11-24 17:38:20 +0400

    Started JS but it is not working.

--- assets/js/animation.js
+++ assets/js/animation.js
@@ -1,12 +1,18 @@
-function animate (){
 
+    let buttons = document.getElementsByClassName("button");
     let days = document.querySelectorAll(".days");
     let slots = document.querySelectorAll(".slots");
 
-    if (days.style.height === "200px" ){
-        days.style.height = "auto";
-        slots.style.display = "block";
-    }
+function animate () {
+    for (let i = 0; i < days.length; i++)
+        if (days[i].style.height === "200px") {
+            days[i].style.height = "auto";
+            for (let j = 0; j < slots.length; j++) {
+                slots[j].style.display = "block";
+            }
+        }
 }
-
-document.querySelectorAll(".button").addEventListener("click", animate);
+    for ( let k=0; k < buttons.length; k++){
+        let button = buttons[k];
+        button.addEventListener("click", animate);
+    }
