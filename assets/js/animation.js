f47610af362aa12906ddcafea0dc32228e2cdf17
tsotne <mail_to_tsotne@yahoo.com>
2020-11-24 22:43:02 +0400

    Started JS but it is not working. Added slot-wrapper

--- assets/js/animation.js
+++ assets/js/animation.js
@@ -1,18 +1,16 @@
-
-    let buttons = document.getElementsByClassName("button");
+    let buttons = document.querySelectorAll(".button");
     let days = document.querySelectorAll(".days");
-    let slots = document.querySelectorAll(".slots");
+    let slots = document.querySelectorAll(".slot-wrapper");
 
 function animate () {
-    for (let i = 0; i < days.length; i++)
-        if (days[i].style.height === "200px") {
-            days[i].style.height = "auto";
-            for (let j = 0; j < slots.length; j++) {
-                slots[j].style.display = "block";
+
+            for (let i = 0; i < days.length; i++) {
+                days[i].style.height = "auto";
+                slots[i].style.display = "block";
             }
-        }
 }
-    for ( let k=0; k < buttons.length; k++){
-        let button = buttons[k];
+
+    for ( let j = 0; j < buttons.length; j++){
+        let button = buttons[j];
         button.addEventListener("click", animate);
     }
